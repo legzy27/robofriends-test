4b40bfd1bb8f67f3bb5506106317239be278c25e
Alex Oyedele <niyo4real2002@yahoo.comm>
2020-05-29 01:04:33 +0100

    prettier

--- src/components/CounterButton.test.js
+++ src/components/CounterButton.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import CounterButton from './CounterButton';
+import CounterButton2 from './CounterButton';
 
 it('renders without crashing', () => {
   expect(shallow(<CounterButton />)).toMatchSnapshot();
